de424bb6cb2be6f45b360371d4001890dbb01930
Diaran Cruz <diaran.real@gmail.com>
2024-09-11 17:34:06 -0300

    Commit 2

--- js/script.js
+++ js/script.js
@@ -18,3 +18,10 @@ function calcAreaCirculo() {
     let area = pi * raio**2
     document.getElementById("area").innerText = area
 }
+
+function soma() {
+    let n1 = Number(document.getElementById("n1").value)
+    let n2 = Number(document.getElementById("n2").value)    
+    let soma = n1 + n2
+    document.getElementById("soma").innerText = soma
+}
